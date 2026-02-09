e9780ee132197981de48ae8f14bc80813f64c69b
correaedisonitm <correaedisonitm@gmail.com>
2026-02-08 19:25:56 -0500

    Primera Subida de archivo, todo con "Commit" - First file upload, all with "Commit"

--- exercise_1/exercise_1/Program.cs
+++ exercise_1/exercise_1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Ingrese un numreo");
+var numberString = Console.ReadLine();
+var numberInt = int.Parse(numberString!);
+if (numberInt %  2 == 0)
+{
+    Console.WriteLine($"El número {numberInt} es par");
+}
+else
+{
+    Console.WriteLine(" El número es ompar");
+}
